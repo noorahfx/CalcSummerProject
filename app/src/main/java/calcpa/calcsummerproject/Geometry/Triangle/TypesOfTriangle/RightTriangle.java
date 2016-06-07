9ac62f774f8337131f88fe9e42de4bedac56bae6
noorahfx <noorahfx@dukes.jmu.edu>
2016-06-07 11:29:58 -0400

    Worked on rigt angle activity UI

--- app/src/main/java/calcpa/calcsummerproject/Geometry/Triangle/TypesOfTriangle/RightTriangle.java
+++ app/src/main/java/calcpa/calcsummerproject/Geometry/Triangle/TypesOfTriangle/RightTriangle.java
@@ -2,16 +2,9 @@ package calcpa.calcsummerproject.Geometry.Triangle.TypesOfTriangle;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.method.ScrollingMovementMethod;
-import android.view.View;
 import android.widget.TextView;
-
-import java.lang.reflect.Type;
-
 import calcpa.calcsummerproject.R;
 import io.github.kexanie.library.MathView;
 
@@ -39,53 +32,96 @@ public class RightTriangle extends AppCompatActivity {
 
         Typeface myTypeFace = Typeface.createFromAsset(getAssets(),"Take cover.ttf");
         TextView myTextView = (TextView)findViewById(R.id.right_triangle_title);
+        assert myTextView != null;
+        myTextView.setTypeface(myTypeFace);
+
+        myTypeFace = Typeface.createFromAsset(getAssets(),"CaviarDreams.ttf");
+        myTextView = (TextView)findViewById(R.id.right_triangle_define_id);
+        assert myTextView != null;
+        myTextView.setTypeface(myTypeFace);
+
+        myTypeFace = Typeface.createFromAsset(getAssets(),"CaviarDreams.ttf");
+        myTextView = (TextView)findViewById(R.id.right_tr_att_1_id);
+        assert myTextView != null;
+        myTextView.setTypeface(myTypeFace);
+
+        //---------------------
+      /**  myTypeFace = Typeface.createFromAsset(getAssets(),"CaviarDreams_Bold.ttf");
+        myTextView = (TextView)findViewById(R.id.right_tr_sides_id);
+        assert myTextView != null;
+        myTextView.setTypeface(myTypeFace);
+
+        myTypeFace = Typeface.createFromAsset(getAssets(),"CaviarDreams_Bold.ttf");
+        myTextView = (TextView)findViewById(R.id.right_tr_hyp_id);
+        assert myTextView != null;
+        myTextView.setTypeface(myTypeFace);
+
+        ///------ */
+
+        myTypeFace = Typeface.createFromAsset(getAssets(),"CaviarDreams.ttf");
+        myTextView = (TextView)findViewById(R.id.right_tr_att_2_id);
+        assert myTextView != null;
+        myTextView.setTypeface(myTypeFace);
+
+
+        myTypeFace = Typeface.createFromAsset(getAssets(),"CaviarDreams.ttf");
+        myTextView = (TextView)findViewById(R.id.right_tr_prop_1_id);
+        assert myTextView != null;
+        myTextView.setTypeface(myTypeFace);
+
+
+        myTypeFace = Typeface.createFromAsset(getAssets(),"CaviarDreams.ttf");
+        myTextView = (TextView)findViewById(R.id.right_tr_prop_2_id);
+        assert myTextView != null;
         myTextView.setTypeface(myTypeFace);
 
         myTypeFace=Typeface.createFromAsset(getAssets(),"[TOP_SECRET].ttf");
         myTextView=(TextView)findViewById(R.id.right_tr_def_str_id);
+        assert myTextView != null;
         myTextView.setTypeface(myTypeFace);
 
         myTypeFace=Typeface.createFromAsset(getAssets(),"[TOP_SECRET].ttf");
         myTextView=(TextView)findViewById(R.id.right_tr_attr_str);
+        assert myTextView != null;
         myTextView.setTypeface(myTypeFace);
 
         myTypeFace=Typeface.createFromAsset(getAssets(),"[TOP_SECRET].ttf");
         myTextView=(TextView)findViewById(R.id.right_tr_prop_id);
+        assert myTextView != null;
         myTextView.setTypeface(myTypeFace);
 
         myTypeFace=Typeface.createFromAsset(getAssets(),"[TOP_SECRET].ttf");
         myTextView=(TextView)findViewById(R.id.right_tri_formula_id);
+        assert myTextView != null;
         myTextView.setTypeface(myTypeFace);
 
-
         TextView tv = (TextView) findViewById(R.id.right_tr_def_str_id);
+        assert tv != null;
         tv.setMovementMethod(new ScrollingMovementMethod());
 
         right_triangle_paramter =(MathView)findViewById(R.id.right_triangle_paramter);
+        assert right_triangle_paramter != null;
         right_triangle_paramter.setText(r_t_paramter);
 
         right_triangle_area = (MathView)findViewById(R.id.right_triangle_area);
+        assert right_triangle_area != null;
         right_triangle_area.setText(r_t_area);
 
         righ_triangle_side_c = (MathView)findViewById(R.id.right_triangle_side_c);
+        assert righ_triangle_side_c != null;
         righ_triangle_side_c.setText(r_t_side_c);
 
         right_triangle_side_a =(MathView)findViewById(R.id.right_triangle_side_a);
+        assert right_triangle_side_a != null;
         right_triangle_side_a.setText(r_t_side_a);
 
         right_triangle_side_b =(MathView)findViewById(R.id.right_triangle_side_b);
+        assert right_triangle_side_b != null;
         right_triangle_side_b.setText(r_t_side_b);
-
-
-
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-
     }
-
-
 }

94f359f757293987116afac36ed258887a3f2902
noorahfx <noorahfx@dukes.jmu.edu>
2016-07-12 12:12:19 -0400

    WOrked on java files and design

--- app/src/main/java/calcpa/calcsummerproject/Geometry/Hexagon/TypesOfHexagon/RegularHexagon.java
+++ app/src/main/java/calcpa/calcsummerproject/Geometry/Hexagon/TypesOfHexagon/RegularHexagon.java
@@ -1,31 +1,31 @@
 package calcpa.calcsummerproject.Geometry.Hexagon.TypesOfHexagon;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
 
 import calcpa.calcsummerproject.R;
 
 public class RegularHexagon extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regular_hexagon);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
+
--- app/src/main/java/calcpa/calcsummerproject/Geometry/Triangle/TypesOfTriangle/RightTriangle.java
+++ app/src/main/java/calcpa/calcsummerproject/Geometry/Triangle/TypesOfTriangle/RightTriangle.java
@@ -3,100 +3,134 @@ package calcpa.calcsummerproject.Geometry.Triangle.TypesOfTriangle;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import calcpa.calcsummerproject.R;
 import io.github.kexanie.library.MathView;
 
+
 public class RightTriangle extends AppCompatActivity {
-    MathView right_triangle_paramter;
-    String r_t_paramter = "$$P=a+b+(\\sqrt{a^2+b^2})$$";
 
-    MathView right_triangle_area;
-    String r_t_area = "$$A=\\frac{ab}{2}$$";
+    EditText sideAEditText;
+    EditText sideBEditText;
+    TextView answerEditText;
 
-    MathView right_triangle_side_a;
-    String r_t_side_a = "$$a = 2 \\frac{A}{b}$$";
+    Button calculateResultButton;
 
-    MathView right_triangle_side_b;
-    String r_t_side_b = "$$b = 2 \\frac{A}{a}$$";
+  //  private double sideA;
+    //private double sideB;
+    //private  final double hypotenuse = Math.sqrt(Math.pow(sideA,2)+ Math.pow(sideB,2));
 
-    MathView righ_triangle_side_c;
-    String r_t_side_c = "$$c=\\sqrt{a^{2}+b^{2}}$$";
+    //right triangle area
+    EditText areaSideAET;
+    EditText areaSideBET;
+    TextView areaAnswerTV;
+    Button calculateAreaButton;
 
 
+
+    /**
+       MathView right_triangle_paramter;
+       String r_t_paramter = "$$P=a+b+(\\sqrt{a^2+b^2})$$";
+
+      MathView right_triangle_area;
+       String r_t_area = "$$A=\\frac{ab}{2}$$";
+
+       MathView right_triangle_side_a;
+       String r_t_side_a = "$$a = 2 \\frac{A}{b}$$";
+
+       MathView right_triangle_side_b;
+       String r_t_side_b = "$$b = 2 \\frac{A}{a}$$";
+
+       MathView righ_triangle_side_c;
+       String r_t_side_c = "$$c=\\sqrt{a^{2}+b^{2}}$$";
+
+   */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_right_triangle);
-
-        Typeface myTypeFace = Typeface.createFromAsset(getAssets(),"Take cover.ttf");
-        TextView myTextView = (TextView)findViewById(R.id.right_triangle_title);
-        assert myTextView != null;
-        myTextView.setTypeface(myTypeFace);
-
-        myTypeFace = Typeface.createFromAsset(getAssets(),"CaviarDreams.ttf");
-        myTextView = (TextView)findViewById(R.id.right_triangle_define_id);
-        assert myTextView != null;
-        myTextView.setTypeface(myTypeFace);
-
-        myTypeFace = Typeface.createFromAsset(getAssets(),"CaviarDreams.ttf");
-        myTextView = (TextView)findViewById(R.id.right_tr_att_1_id);
-        assert myTextView != null;
-        myTextView.setTypeface(myTypeFace);
-
-
-
-
-        myTypeFace = Typeface.createFromAsset(getAssets(),"CaviarDreams.ttf");
-        myTextView = (TextView)findViewById(R.id.right_tr_prop_1_id);
-        assert myTextView != null;
-        myTextView.setTypeface(myTypeFace);
-
-        myTypeFace=Typeface.createFromAsset(getAssets(),"[TOP_SECRET].ttf");
-        myTextView=(TextView)findViewById(R.id.right_tr_def_str_id);
-        assert myTextView != null;
-        myTextView.setTypeface(myTypeFace);
-
-        myTypeFace=Typeface.createFromAsset(getAssets(),"[TOP_SECRET].ttf");
-        myTextView=(TextView)findViewById(R.id.right_tr_attr_str);
-        assert myTextView != null;
-        myTextView.setTypeface(myTypeFace);
-
-        myTypeFace=Typeface.createFromAsset(getAssets(),"[TOP_SECRET].ttf");
-        myTextView=(TextView)findViewById(R.id.right_tr_prop_id);
-        assert myTextView != null;
-        myTextView.setTypeface(myTypeFace);
-
-        myTypeFace=Typeface.createFromAsset(getAssets(),"[TOP_SECRET].ttf");
-        myTextView=(TextView)findViewById(R.id.right_tri_formula_id);
-        assert myTextView != null;
-        myTextView.setTypeface(myTypeFace);
-
-        TextView tv = (TextView) findViewById(R.id.right_tr_def_str_id);
-        assert tv != null;
-        tv.setMovementMethod(new ScrollingMovementMethod());
-
-        right_triangle_paramter =(MathView)findViewById(R.id.right_triangle_paramter);
+/**
+        right_triangle_paramter = (MathView)findViewById(R.id.right_triangle_perimeter_formula);
         assert right_triangle_paramter != null;
         right_triangle_paramter.setText(r_t_paramter);
+**/
+        Typeface myTypeFace = Typeface.createFromAsset(getAssets(),"Take cover.ttf");
+        TextView myTextView = (TextView)findViewById(R.id.perimeter_text);
+        assert myTextView != null;
+        myTextView.setTypeface(myTypeFace);
 
-        right_triangle_area = (MathView)findViewById(R.id.right_triangle_area);
-        assert right_triangle_area != null;
-        right_triangle_area.setText(r_t_area);
 
-        righ_triangle_side_c = (MathView)findViewById(R.id.right_triangle_side_c);
-        assert righ_triangle_side_c != null;
-        righ_triangle_side_c.setText(r_t_side_c);
+        //initialize the EditText
 
-        right_triangle_side_a =(MathView)findViewById(R.id.right_triangle_side_a);
-        assert right_triangle_side_a != null;
-        right_triangle_side_a.setText(r_t_side_a);
+        sideAEditText = (EditText) findViewById(R.id.sidAEditText);
+        sideBEditText = (EditText) findViewById(R.id.sidBEditText);
+        answerEditText = (TextView) findViewById(R.id.answerEditText);
+        calculateResultButton = (Button) findViewById(R.id.calculateButtonID);
+        calculateResultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        right_triangle_side_b =(MathView)findViewById(R.id.right_triangle_side_b);
-        assert right_triangle_side_b != null;
-        right_triangle_side_b.setText(r_t_side_b);
+                String sideA = sideAEditText.getText().toString();
+                String sideB = sideBEditText.getText().toString();
+
+                double perimeter = Double.parseDouble(sideA) + Double.parseDouble(sideB) +
+                        Math.sqrt(Math.pow(Double.parseDouble(sideA), 2) +
+                                Math.pow(Double.parseDouble(sideB), 2));
+                answerEditText.setText(String.format("%.02f",perimeter));
+
+
+
+                if (TextUtils.isEmpty(sideA)) {
+                    Toast.makeText(getApplicationContext(), "Enter height", Toast.LENGTH_SHORT).show();
+
+                }
+                if (TextUtils.isEmpty(sideB)) {
+                    Toast.makeText(getApplicationContext(), "Enter base", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+            }
+        });
+
+        areaSideAET = (EditText)findViewById(R.id.area_base_et);
+        areaSideBET = (EditText)findViewById(R.id.area_height_et);
+        areaAnswerTV = (TextView)findViewById(R.id.area_answer_tv);
+        calculateAreaButton = (Button)findViewById(R.id.area_click_botton);
+        calculateAreaButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v){
+                String sideA = areaSideAET.getText().toString();
+                String sideB =areaSideBET.getText().toString();
+
+                double area = (Double.parseDouble(sideA)*Double.parseDouble(sideB))/2;
+                areaAnswerTV.setText(String.format(".02f",area));
+
+                if (TextUtils.isEmpty(sideA)) {
+                    Toast.makeText(getApplicationContext(), "Enter height", Toast.LENGTH_SHORT).show();
+
+                }
+                if (TextUtils.isEmpty(sideB)) {
+                    Toast.makeText(getApplicationContext(), "Enter base", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+            }
+
+        });
+
+
+
+
     }
+
 
     @Override
     protected void onResume() {

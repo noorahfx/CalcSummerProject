14493693192a30e430db9028ed8e1b1833dc40bb
noorahfx <noorahfx@dukes.jmu.edu>
2016-07-12 13:22:13 -0400

    Right triangle xml file and java codes added

--- app/src/main/java/calcpa/calcsummerproject/Geometry/Triangle/TypesOfTriangle/RightTriangle.java
+++ app/src/main/java/calcpa/calcsummerproject/Geometry/Triangle/TypesOfTriangle/RightTriangle.java
@@ -4,28 +4,20 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import calcpa.calcsummerproject.R;
-import io.github.kexanie.library.MathView;
-
 
 public class RightTriangle extends AppCompatActivity {
 
     EditText sideAEditText;
     EditText sideBEditText;
     TextView answerEditText;
-
     Button calculateResultButton;
 
-  //  private double sideA;
-    //private double sideB;
-    //private  final double hypotenuse = Math.sqrt(Math.pow(sideA,2)+ Math.pow(sideB,2));
 
     //right triangle area
     EditText areaSideAET;
@@ -33,42 +25,23 @@ public class RightTriangle extends AppCompatActivity {
     TextView areaAnswerTV;
     Button calculateAreaButton;
 
+    //right triangle hypotenuse
+    EditText hypSideAET;
+    EditText hypSideBET;
+    TextView hypAnswerTV;
+    Button calculateHypButton;
 
-
-    /**
-       MathView right_triangle_paramter;
-       String r_t_paramter = "$$P=a+b+(\\sqrt{a^2+b^2})$$";
-
-      MathView right_triangle_area;
-       String r_t_area = "$$A=\\frac{ab}{2}$$";
-
-       MathView right_triangle_side_a;
-       String r_t_side_a = "$$a = 2 \\frac{A}{b}$$";
-
-       MathView right_triangle_side_b;
-       String r_t_side_b = "$$b = 2 \\frac{A}{a}$$";
-
-       MathView righ_triangle_side_c;
-       String r_t_side_c = "$$c=\\sqrt{a^{2}+b^{2}}$$";
-
-   */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_right_triangle);
-/**
-        right_triangle_paramter = (MathView)findViewById(R.id.right_triangle_perimeter_formula);
-        assert right_triangle_paramter != null;
-        right_triangle_paramter.setText(r_t_paramter);
-**/
+
         Typeface myTypeFace = Typeface.createFromAsset(getAssets(),"Take cover.ttf");
         TextView myTextView = (TextView)findViewById(R.id.perimeter_text);
         assert myTextView != null;
         myTextView.setTypeface(myTypeFace);
 
-
         //initialize the EditText
-
         sideAEditText = (EditText) findViewById(R.id.sidAEditText);
         sideBEditText = (EditText) findViewById(R.id.sidBEditText);
         answerEditText = (TextView) findViewById(R.id.answerEditText);
@@ -85,18 +58,13 @@ public class RightTriangle extends AppCompatActivity {
                                 Math.pow(Double.parseDouble(sideB), 2));
                 answerEditText.setText(String.format("%.02f",perimeter));
 
-
-
                 if (TextUtils.isEmpty(sideA)) {
-                    Toast.makeText(getApplicationContext(), "Enter height", Toast.LENGTH_SHORT).show();
-
+                    Toast.makeText(getApplicationContext(),
+                            "Enter height", Toast.LENGTH_SHORT).show();
                 }
                 if (TextUtils.isEmpty(sideB)) {
                     Toast.makeText(getApplicationContext(), "Enter base", Toast.LENGTH_SHORT).show();
-                    return;
                 }
-
-
             }
         });
 
@@ -114,21 +82,40 @@ public class RightTriangle extends AppCompatActivity {
                 areaAnswerTV.setText(String.format(".02f",area));
 
                 if (TextUtils.isEmpty(sideA)) {
-                    Toast.makeText(getApplicationContext(), "Enter height", Toast.LENGTH_SHORT).show();
-
+                    Toast.makeText(getApplicationContext(),
+                            "Enter height", Toast.LENGTH_SHORT).show();
                 }
                 if (TextUtils.isEmpty(sideB)) {
-                    Toast.makeText(getApplicationContext(), "Enter base", Toast.LENGTH_SHORT).show();
-                    return;
+                    Toast.makeText(getApplicationContext(),
+                            "Enter base", Toast.LENGTH_SHORT).show();
                 }
-
             }
-
         });
 
+        hypSideAET =(EditText)findViewById(R.id.hyp_side_a_et);
+        hypSideBET =(EditText)findViewById(R.id.hyp_side_b_et);
+        hypAnswerTV=(TextView)findViewById(R.id.hyp_answer_tv);
+        calculateHypButton =(Button)findViewById(R.id.hyp_calculate_button);
+        calculateHypButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sideA = areaSideAET.getText().toString();
+                String sideB =areaSideBET.getText().toString();
 
+                double hyp = Math.sqrt(Math.pow(Double.parseDouble(sideA),2)+
+                        Math.pow(Double.parseDouble(sideB),2));
+                areaAnswerTV.setText(String.format(".02f",hyp));
 
-
+                if (TextUtils.isEmpty(sideA)) {
+                    Toast.makeText(getApplicationContext(),
+                            "Enter height", Toast.LENGTH_SHORT).show();
+                }
+                if (TextUtils.isEmpty(sideB)) {
+                    Toast.makeText(getApplicationContext(),
+                            "Enter base", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 

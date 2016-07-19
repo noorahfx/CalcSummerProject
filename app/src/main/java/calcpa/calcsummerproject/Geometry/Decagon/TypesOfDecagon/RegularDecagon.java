9f394c9d571a1c7d081fec9b718bc66b1f59e83e
noorahfx <noorahfx@dukes.jmu.edu>
2016-07-19 16:18:33 -0400

    Decagon activity connected with the XML layout

--- app/src/main/java/calcpa/calcsummerproject/Geometry/Decagon/TypesOfDecagon/RegularDecagon.java
+++ app/src/main/java/calcpa/calcsummerproject/Geometry/Decagon/TypesOfDecagon/RegularDecagon.java
@@ -5,11 +5,35 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import calcpa.calcsummerproject.R;
 
 public class RegularDecagon extends AppCompatActivity {
+    //area
+    EditText decagonareaET;
+    TextView decagonareaTV;
+    Button decagonareaCalcButton;
+
+
+    //perimeter
+    EditText decagonPerimeterET;
+    TextView decagonPerimeterTV;
+    Button decagonPerimeterCalcButton;
+
+
+    //Sides
+
+    EditText decagonSideET;
+    TextView decagonSideTV;
+    Button decagonSideCalcButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +42,87 @@ public class RegularDecagon extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        //area
+        decagonareaET = (EditText) findViewById(R.id.decagon_area_a_et);
+        decagonareaTV = (TextView) findViewById(R.id.decagon_area_calc_answer_tx);
+        decagonareaCalcButton = (Button) findViewById(R.id.decagon_area_calc_button);
+        decagonareaCalcButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
+                /**
+                 * work on functionality
+                 * formula is not integrated yet
+                 */
+                String sides = decagonareaET.getText().toString();
+
+
+                double area = 6 * Double.parseDouble(sides);
+                decagonareaTV.setText(String.format("%.02f", area));
+
+                if (TextUtils.isEmpty(sides)) {
+                    Toast.makeText(getApplicationContext(),
+                            "Enter height", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
+
+        //perimeter
+        decagonPerimeterET = (EditText) findViewById(R.id.decagon_perimeter_v_et);
+        decagonPerimeterTV = (TextView) findViewById(R.id.decagon_perimeter_calc_answer_tx);
+        decagonPerimeterCalcButton = (Button) findViewById(R.id.decagon_perimeter_calc_button);
+        decagonPerimeterCalcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /**
+                 * work on functionality
+                 * formula is not integrated yet
+                 */
+                String area = decagonPerimeterET.getText().toString();
+
+
+                double perimeter = 6 * Double.parseDouble(area);
+                decagonPerimeterTV.setText(String.format("%.02f", perimeter));
+
+                if (TextUtils.isEmpty(area)) {
+                    Toast.makeText(getApplicationContext(),
+                            "Enter height", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+        //sides
+
+        decagonSideET = (EditText) findViewById(R.id.decagon_sides_a_et);
+        decagonSideTV = (TextView) findViewById(R.id.decagon_sides_calc_answer_tx);
+        decagonSideCalcButton = (Button) findViewById(R.id.decagon_sides_calc_button);
+        decagonSideCalcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /**
+                 * work on functionality
+                 * formula is not integrated yet
+                 */
+                String area = decagonSideET.getText().toString();
+
+
+                double diagonal = 6 * Double.parseDouble(area);
+                decagonSideTV.setText(String.format("%.02f", diagonal));
+
+                if (TextUtils.isEmpty(area)) {
+                    Toast.makeText(getApplicationContext(),
+                            "Enter height", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 
 }

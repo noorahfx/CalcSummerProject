41453665016ee2f4e762e063389bf0aaa56f39ef
noorahfx <noorahfx@dukes.jmu.edu>
2016-07-26 10:50:14 -0400

    Debugged and fixed Cone activity functions

--- app/src/main/java/calcpa/calcsummerproject/Geometry/Cone/Cone.java
+++ app/src/main/java/calcpa/calcsummerproject/Geometry/Cone/Cone.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import calcpa.calcsummerproject.Model;
 import calcpa.calcsummerproject.R;
 
 public class Cone extends AppCompatActivity {
@@ -67,207 +68,184 @@ public class Cone extends AppCompatActivity {
 
 
         //Volume
-        coneVolumeRET = (EditText)findViewById(R.id.cone_volume_r_et);
-        coneVolumeHET = (EditText)findViewById(R.id.cone_volume_h_et);
-        coneVolumeAnswerTV = (TextView)findViewById(R.id.cone_volume_calc_answer_tx);
-        coneVolumeCalcButton =(Button)findViewById(R.id.cone_volume_calc_button);
+        coneVolumeRET = (EditText) findViewById(R.id.cone_volume_r_et);
+        coneVolumeHET = (EditText) findViewById(R.id.cone_volume_h_et);
+        coneVolumeAnswerTV = (TextView) findViewById(R.id.cone_volume_calc_answer_tx);
+        coneVolumeCalcButton = (Button) findViewById(R.id.cone_volume_calc_button);
         coneVolumeCalcButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                /**
-                 * work on functionality
-                 * formula is not integrated yet
-                  */
-                String height = coneVolumeRET.getText().toString();
-                String volume = coneVolumeRET.getText().toString();
+                //check to make sure field is not empty
+                if (Model.isEmpty(coneVolumeRET)) {
+                    coneVolumeRET.setError("Enter Radius");
+                } else if (Model.isEmpty(coneVolumeHET)) {
+                    coneVolumeHET.setError("Enter Height");
+                } else {
 
-
-
-                double area = 6 * Double.parseDouble(height);
-                coneVolumeAnswerTV.setText(String.format("%.02f", area));
-
-                if (TextUtils.isEmpty(height)) {
-                    Toast.makeText(getApplicationContext(),
-                            "Enter height", Toast.LENGTH_SHORT).show();
+                    double radius, height, answer;
+                    radius = Double.parseDouble(coneVolumeRET.getText().toString());
+                    height = Double.parseDouble(coneVolumeHET.getText().toString());
+                    answer = Math.PI * (radius * radius) * (height / 3);
+                    coneVolumeAnswerTV.setText(String.format("%.02f", answer));
                 }
+
             }
         });
 
         //Radius
-        coneRadiusHET = (EditText)findViewById(R.id.cone_radius_h_et);
-        coneRadiusVET = (EditText)findViewById(R.id.cone_radius_v_et);
-        coneRadiusAnswerTV = (TextView)findViewById(R.id.cone_radius_calc_answer_tx);
-        coneRadiusCalcButton =(Button)findViewById(R.id.cone_radius_calc_button);
+        coneRadiusHET = (EditText) findViewById(R.id.cone_radius_h_et);
+        coneRadiusVET = (EditText) findViewById(R.id.cone_radius_v_et);
+        coneRadiusAnswerTV = (TextView) findViewById(R.id.cone_radius_calc_answer_tx);
+        coneRadiusCalcButton = (Button) findViewById(R.id.cone_radius_calc_button);
         coneRadiusCalcButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                /**
-                 * work on functionality
-                 * formula is not integrated yet
-                 */
-                String height = coneRadiusHET.getText().toString();
-                String volume = coneRadiusVET.getText().toString();
+                //check to make sure field is not empty
+                if (Model.isEmpty(coneRadiusHET)) {
+                    coneRadiusHET.setError("Enter Height");
+                } else if (Model.isEmpty(coneRadiusVET)) {
+                    coneRadiusVET.setError("Enter Volume");
+                } else {
 
-
-
-                double area = 6 * Double.parseDouble(height);
-                coneRadiusAnswerTV.setText(String.format("%.02f", area));
-
-                if (TextUtils.isEmpty(height)) {
-                    Toast.makeText(getApplicationContext(),
-                            "Enter height", Toast.LENGTH_SHORT).show();
+                    double height, volume, answer;
+                    height = Double.parseDouble(coneRadiusHET.getText().toString());
+                    volume = Double.parseDouble(coneRadiusVET.getText().toString());
+                    answer = Math.sqrt(3 * (volume / (Math.PI * height)));
+                    coneRadiusAnswerTV.setText(String.format("%.02f", answer));
                 }
+
             }
         });
 
 
-
         //Height
-        coneHeightRET = (EditText)findViewById(R.id.cone_height_r_et);
-        coneHeightVET = (EditText)findViewById(R.id.cone_height_v_et);
-        coneHeightAnswerTV = (TextView)findViewById(R.id.cone_height_calc_answer_tx);
-        coneHeightCalcButton =(Button)findViewById(R.id.cone_height_calc_button);
+        coneHeightRET = (EditText) findViewById(R.id.cone_height_r_et);
+        coneHeightVET = (EditText) findViewById(R.id.cone_height_v_et);
+        coneHeightAnswerTV = (TextView) findViewById(R.id.cone_height_calc_answer_tx);
+        coneHeightCalcButton = (Button) findViewById(R.id.cone_height_calc_button);
         coneHeightCalcButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                /**
-                 * work on functionality
-                 * formula is not integrated yet
-                 */
-                String height = coneHeightRET.getText().toString();
-                String volume = coneHeightVET.getText().toString();
+                //check to make sure field is not empty
+                if (Model.isEmpty(coneHeightRET)) {
+                    coneHeightRET.setError("Enter Radius");
+                } else if (Model.isEmpty(coneHeightVET)) {
+                    coneHeightVET.setError("Enter Volume");
+                } else {
 
-
-
-                double area = 6 * Double.parseDouble(height);
-                coneHeightAnswerTV.setText(String.format("%.02f", area));
-
-                if (TextUtils.isEmpty(height)) {
-                    Toast.makeText(getApplicationContext(),
-                            "Enter height", Toast.LENGTH_SHORT).show();
+                    double radius, volume, answer;
+                    radius = Double.parseDouble(coneHeightRET.getText().toString());
+                    volume = Double.parseDouble(coneHeightVET.getText().toString());
+                    answer = (3 * (volume / (Math.PI * (radius * radius))));
+                    coneHeightAnswerTV.setText(String.format("%.02f", answer));
                 }
             }
         });
 
         //Slant height
-        coneSlantHeightRET = (EditText)findViewById(R.id.cone_slant_height_r_et);
-        coneSLantHeightHET = (EditText)findViewById(R.id.cone_slant_height_h_et);
-        coneSlantHeightAnswerTV = (TextView)findViewById(R.id.cone_slant_height_calc_answer_tx);
-        coneSlantHeightCalcButton =(Button)findViewById(R.id.cone_slant_height_calc_button);
+        coneSlantHeightRET = (EditText) findViewById(R.id.cone_slant_height_r_et);
+        coneSLantHeightHET = (EditText) findViewById(R.id.cone_slant_height_h_et);
+        coneSlantHeightAnswerTV = (TextView) findViewById(R.id.cone_slant_height_calc_answer_tx);
+        coneSlantHeightCalcButton = (Button) findViewById(R.id.cone_slant_height_calc_button);
         coneSlantHeightCalcButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                /**
-                 * work on functionality
-                 * formula is not integrated yet
-                 */
-                String height = coneSlantHeightRET.getText().toString();
-                String volume = coneSLantHeightHET.getText().toString();
+                //check to make sure field is not empty
+                if (Model.isEmpty(coneSlantHeightRET)) {
+                    coneSlantHeightRET.setError("Enter Radius");
+                } else if (Model.isEmpty(coneSLantHeightHET)) {
+                    coneSLantHeightHET.setError("Enter Height");
+                } else {
 
-
-
-                double area = 6 * Double.parseDouble(height);
-                coneSlantHeightAnswerTV.setText(String.format("%.02f", area));
-
-                if (TextUtils.isEmpty(height)) {
-                    Toast.makeText(getApplicationContext(),
-                            "Enter height", Toast.LENGTH_SHORT).show();
+                    double radius, height, answer;
+                    radius = Double.parseDouble(coneSlantHeightRET.getText().toString());
+                    height = Double.parseDouble(coneSLantHeightHET.getText().toString());
+                    answer = Math.sqrt((radius * radius) + (height * height));
+                    coneSlantHeightAnswerTV.setText(String.format("%.02f", answer));
                 }
             }
         });
 
 
         //Surface Area
-        coneSurfaceAreatRET = (EditText)findViewById(R.id.cone_surface_area_r_et);
-        coneSurfaceAreaHET = (EditText)findViewById(R.id.cone_surface_area_h_et);
-        coneSurfaceAreaAnswerTV = (TextView)findViewById(R.id.cone_surface_area_calc_answer_tx);
-        coneSurfaceAreaCalcButton =(Button)findViewById(R.id.cone_surface_area_calc_button);
+        coneSurfaceAreatRET = (EditText) findViewById(R.id.cone_surface_area_r_et);
+        coneSurfaceAreaHET = (EditText) findViewById(R.id.cone_surface_area_h_et);
+        coneSurfaceAreaAnswerTV = (TextView) findViewById(R.id.cone_surface_area_calc_answer_tx);
+        coneSurfaceAreaCalcButton = (Button) findViewById(R.id.cone_surface_area_calc_button);
         coneSurfaceAreaCalcButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //check to make sure field is not empty
+                if (Model.isEmpty(coneSurfaceAreatRET)) {
+                    coneSurfaceAreatRET.setError("Enter Radius");
+                } else if (Model.isEmpty(coneSurfaceAreaHET)) {
+                    coneSurfaceAreaHET.setError("Enter Height");
+                } else {
 
-                /**
-                 * work on functionality
-                 * formula is not integrated yet
-                 */
-                String height = coneSurfaceAreatRET.getText().toString();
-                String volume = coneSurfaceAreaHET.getText().toString();
-
-
-
-                double area = 6 * Double.parseDouble(height);
-                coneSurfaceAreaAnswerTV.setText(String.format("%.02f", area));
-
-                if (TextUtils.isEmpty(height)) {
-                    Toast.makeText(getApplicationContext(),
-                            "Enter height", Toast.LENGTH_SHORT).show();
+                    double radius, height, answer;
+                    radius = Double.parseDouble(coneSurfaceAreatRET.getText().toString());
+                    height = Double.parseDouble(coneSurfaceAreaHET.getText().toString());
+                    answer = (Math.PI * radius * (radius + Math.sqrt((height * height) + (radius * radius))));
+                    coneSurfaceAreaAnswerTV.setText(String.format("%.02f", answer));
                 }
+
+
             }
         });
-
 
 
         //Base Area
-        coneBaseAreaRET = (EditText)findViewById(R.id.cone_base_area_r_et);
-        coneBaseAreaAnswerTV = (TextView)findViewById(R.id.cone_base_area_calc_answer_tx);
-        coneBaseAreaCalcButton =(Button)findViewById(R.id.cone_base_area_calc_button);
+        coneBaseAreaRET = (EditText) findViewById(R.id.cone_base_area_r_et);
+        coneBaseAreaAnswerTV = (TextView) findViewById(R.id.cone_base_area_calc_answer_tx);
+        coneBaseAreaCalcButton = (Button) findViewById(R.id.cone_base_area_calc_button);
         coneBaseAreaCalcButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //check to make sure field is not empty
+                if (Model.isEmpty(coneBaseAreaRET)) {
+                    coneBaseAreaRET.setError("Enter Radius");
+                } else {
 
-                /**
-                 * work on functionality
-                 * formula is not integrated yet
-                 */
-                String height = coneBaseAreaRET.getText().toString();
-
-
-
-                double area = 6 * Double.parseDouble(height);
-                coneBaseAreaAnswerTV.setText(String.format("%.02f", area));
-
-                if (TextUtils.isEmpty(height)) {
-                    Toast.makeText(getApplicationContext(),
-                            "Enter height", Toast.LENGTH_SHORT).show();
+                    double radius, answer;
+                    radius = Double.parseDouble(coneBaseAreaRET.getText().toString());
+                    answer = Math.PI * (radius * radius);
+                    coneBaseAreaAnswerTV.setText(String.format("%.02f", answer));
                 }
+
             }
+
         });
-
-
 
 
         //Lateral Surface
-        coneLateralSurfaceRET= (EditText)findViewById(R.id.cone_lateral_surface_r_et);
-        coneLateralSurfaceHET = (EditText)findViewById(R.id.cone_lateral_surface_h_et);
-        coneLateralSurfaceAnswerTV = (TextView)findViewById(R.id.cone_lateral_surface_calc_answer_tx);
-        coneLateralSurfaceCalcButton =(Button)findViewById(R.id.cone_lateral_surface_calc_button);
+        coneLateralSurfaceRET = (EditText) findViewById(R.id.cone_lateral_surface_r_et);
+        coneLateralSurfaceHET = (EditText) findViewById(R.id.cone_lateral_surface_h_et);
+        coneLateralSurfaceAnswerTV = (TextView) findViewById(R.id.cone_lateral_surface_calc_answer_tx);
+        coneLateralSurfaceCalcButton = (Button) findViewById(R.id.cone_lateral_surface_calc_button);
         coneLateralSurfaceCalcButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //check to make sure field is not empty
+                if (Model.isEmpty(coneLateralSurfaceRET)) {
+                    coneLateralSurfaceRET.setError("Enter Radius");
+                } else if (Model.isEmpty(coneLateralSurfaceHET)) {
+                    coneLateralSurfaceHET.setError("Enter Height");
+                } else {
 
-                /**
-                 * work on functionality
-                 * formula is not integrated yet
-                 */
-                String height = coneLateralSurfaceRET.getText().toString();
-                String volume = coneLateralSurfaceHET.getText().toString();
-
-
-
-                double area = 6 * Double.parseDouble(height);
-                coneLateralSurfaceAnswerTV.setText(String.format("%.02f", area));
-
-                if (TextUtils.isEmpty(height)) {
-                    Toast.makeText(getApplicationContext(),
-                            "Enter height", Toast.LENGTH_SHORT).show();
+                    double radius, height, answer;
+                    radius = Double.parseDouble(coneLateralSurfaceRET.getText().toString());
+                    height = Double.parseDouble(coneLateralSurfaceHET.getText().toString());
+                    answer = Math.PI * radius * (Math.sqrt((height * height) + (radius * radius)));
+                    coneLateralSurfaceAnswerTV.setText(String.format("%.02f", answer));
                 }
+
+
             }
         });
-
 
 
     }
